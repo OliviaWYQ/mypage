2352752045e75998216f2b9e74cbb481529d6651
Olivia Wang <oliviawang@OliviadeMBP.fios-router.home>
2019-03-26 14:47:15 -0400

    first edison

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -27,14 +27,14 @@ class Header extends Component {
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
          </ul>
 
       </nav>
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">Hi, I'm {name}.</h1>
+            <h1 className="responsive-headline">Hi, I'm Olivia</h1>
             <h3>{description}</h3>
             <hr />
             <ul className="social">
