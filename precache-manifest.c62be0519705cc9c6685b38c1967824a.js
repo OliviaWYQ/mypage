71f9b4e792ec9918be69a33436e577b24e92faa3
Olivia Wang <oliviawang@OliviadeMBP.fios-router.home>
2019-03-26 14:56:17 -0400

    Updates

---
+++ precache-manifest.c62be0519705cc9c6685b38c1967824a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d0eb0fb2dd27e69d724",
+    "url": "/mypage/static/js/runtime~main.4c2b4ee8.js"
+  },
+  {
+    "revision": "1952dcaab1cf861e7c41",
+    "url": "/mypage/static/js/main.667d4b25.chunk.js"
+  },
+  {
+    "revision": "5b4a1aa96faa3628970a",
+    "url": "/mypage/static/js/2.292d2608.chunk.js"
+  },
+  {
+    "revision": "1952dcaab1cf861e7c41",
+    "url": "/mypage/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "fda1da825f474d2318c1af2fe9821011",
+    "url": "/mypage/index.html"
+  }
+];
